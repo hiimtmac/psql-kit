da930acf621539e87421d9977a2859b6e27bc176
hiimtmac <hiimtmac+github@gmail.com>
2021-01-22 15:43:28 -0600

    dates for UTC testing

--- Tests/PSQLKitTests/BindTests.swift
+++ Tests/PSQLKitTests/BindTests.swift
@@ -72,20 +72,21 @@ final class BindTests: PSQLTestCase {
     }
     
     func testDateBindCodable() throws {
-        let swift = DateComponents(calendar: .current, year: 2020, month: 01, day: 01).date!
+        let swift = DateComponents(calendar: .current, timeZone: TimeZone(identifier: "UTC"), year: 2020, month: 01, day: 01, hour: 01, minute: 01, second: 01).date!
+        print(swift)
         
         let date = PSQLDate(swift)
         let timestamp = PSQLTimestamp(swift)
         
         let encodeDate = try JSONEncoder().encode(date)
-        XCTAssertEqual(String(decoding: encodeDate, as: UTF8.self), "599551200")
+        XCTAssertEqual(String(decoding: encodeDate, as: UTF8.self), "599533261")
         let encodeTimestamp = try JSONEncoder().encode(timestamp)
-        XCTAssertEqual(String(decoding: encodeTimestamp, as: UTF8.self), "599551200")
+        XCTAssertEqual(String(decoding: encodeTimestamp, as: UTF8.self), "599533261")
         
         let decodeDate = try JSONDecoder().decode(PSQLDate.self, from: encodeDate)
         XCTAssertEqual("\(decodeDate.storage)", "2020-01-01 00:00:00 +0000")
         let decodeTimestamp = try JSONDecoder().decode(PSQLTimestamp.self, from: encodeTimestamp)
-        XCTAssertEqual("\(decodeTimestamp.storage)", "2020-01-01 06:00:00 +0000")
+        XCTAssertEqual("\(decodeTimestamp.storage)", "2020-01-01 01:01:00 +0000")
     }
     
     func testBindArray() throws {
